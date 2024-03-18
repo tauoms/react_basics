0cc67c761094f28ca72a229222c9392c77b8a6b1
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-18 12:58:03 +0200

    Create basic greeting on main.jsx

--- 01week/react_demo/src/main.jsx
+++ 01week/react_demo/src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const name = "Tuomas Kohvakka";
+const first_page = 2002;
+
+const demo = (
+  <div>
+    <h1>Hello, my name is {name}</h1>
+    <p>I have {2024 - first_page} years of experience building websites.</p>
+  </div>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+{demo}
   </React.StrictMode>,
 )
