937ca147d573df841cf67378624fd0da6ce9a048
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-18 13:03:50 +0200

    Make greeting into a component

--- 01week/react_demo/src/main.jsx
+++ 01week/react_demo/src/main.jsx
@@ -6,15 +6,17 @@ import './index.css'
 const name = "Tuomas Kohvakka";
 const first_page = 2002;
 
-const demo = (
+const Demo = () => { // This is a component
+  return (
   <div>
     <h1>Hello, my name is {name}</h1>
     <p>I have {2024 - first_page} years of experience building websites.</p>
   </div>
-);
+)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-{demo}
+<Demo />
   </React.StrictMode>,
 )
