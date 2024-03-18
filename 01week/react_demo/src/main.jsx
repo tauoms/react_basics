9b732812ed21eda8eb09202a5fb577358c21b6cc
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-18 14:13:44 +0200

    Add Box component, use inside Demo component

--- 01week/react_demo/src/main.jsx
+++ 01week/react_demo/src/main.jsx
@@ -11,12 +11,26 @@ const Demo = () => { // This is a component
   <div>
     <h1>Hello, my name is {name}</h1>
     <p>I have {2024 - first_page} years of experience building websites.</p>
+    <Box name="Jeeves" title="CEO" age="77"/>
+    <Box name="Hannah" title="CTO" age="34" />
+    <Box name="Steven" title="UFO" age="65" />
   </div>
-)
+  )
 }
+
+const Box = (props) => { // Must use term "props"
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Title: {props.title}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<Demo />
+    <Demo />
   </React.StrictMode>,
 )
